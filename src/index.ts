bec2e89da025cfd556fdf9ec3f40508e05b66d78
Christiaan Kruger <christiaank@globalkinetic.com>
2021-11-18 11:22:11 +0200

    Use convict for environment-aware config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import http from 'http';
+import { config } from '../config/config';
 import { app } from './app';
 
-const port = 3000;
+const port = config.get('port');
 
-http.createServer(app).listen(port);
+http.createServer(app).listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
