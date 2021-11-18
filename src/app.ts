9a76527aa9116a36605147ed10fb2659feac672a
Christiaan Kruger <christiaank@globalkinetic.com>
2021-11-18 11:09:35 +0200

    Add security packages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 export const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
+
+app.use(limiter);
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
